f05a0739f9ca9845bbe8e1b6b599ff0fde229205
SebastianEsco <sebastian.escobarm@upb.edu.co>
2024-11-07 12:35:35 -0500

    Primer efecto mixamoCharacter

---
+++ FinalCG/Assets/Scripts/EffectManager.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EffectManager : MonoBehaviour
+{
+
+
+    
+    void Start()
+    {
+        
+    }
+
+
+    public void ReproducirEfecto(int indexEfecto)
+    {
+
+    }
+
+    
+    void Update()
+    {
+        
+    }
+}
